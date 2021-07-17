673003149f84ae89620ece53e62557f6501031cb
Nelson Klutse <1698197+azumah@users.noreply.github.com>
2021-07-17 22:58:06 +0000

    add custom shapes

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,8 +9,40 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        CustomShape()
+        .fill(Color.secondary)
+            .frame(width: 360, height: 350)
+
+    }
+}
+
+
+struct CustomShape: Shape {
+    func path(in rect: CGRect) -> Path {
+        Path { path in
+            path.move(to: CGPoint(x: rect.minX, y: rect.minY))
+            path.addQuadCurve(to: CGPoint(x: rect.minX, y: rect.maxY), control: CGPoint(x: rect.midX, y: rect.midY))
+            path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+            path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+            path.closeSubpath()
+            
+        }
+    }
+    
+
+}
+
+struct StrokeFillView: View {
+    var body: some View {
+        Circle()
+            .fill(Color.pink)
+            .overlay(
+                Circle()
+                    .stroke(lineWidth: 5)
+                    .foregroundColor(.purple)
+            )
+            .frame(width: 300, height: 300)
     }
 }
 
