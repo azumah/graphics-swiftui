c60629464d708a6aa4ad74d20d3b82da29007754
Nelson Klutse <1698197+azumah@users.noreply.github.com>
2021-07-17 22:21:30 +0000

    Initial Commit

---
+++ Shared/graphics_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  graphics_swiftuiApp.swift
+//  Shared
+//
+//  Created by Azu on 17/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct graphics_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
